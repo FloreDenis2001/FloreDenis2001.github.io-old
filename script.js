ffc6bf21578ac30a293228156fd16ab48409cb7b
FloreDenis2001 <floredenis907@yahoo.com>
2023-05-12 03:33:19 +0300

    last_update

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let phoneMenu=document.querySelector(".cover-menu");
 let navBar=document.querySelector(".nav-bar");
+let links=document.querySelectorAll(".nav-link");
+let contactPhone=document.querySelectorAll(".contact");
+
+contactPhone.forEach((contactPhone)=>{
+    
+    contactPhone.addEventListener("click",(e)=>{
+        let childNode = contactPhone.children[1];
+        if(childNode.style.display==="none"){
+           childNode.style.display="flex";
+           setTimeout(() => {
+            childNode.style.display = "none";
+          }, 3000);
+        }else {
+            childNode.style.display="none";
+        }
+    })
+  
+})
+
 
 phoneMenu.addEventListener("click",(e)=>{
     if(navBar.style.display==="none"){
@@ -8,3 +27,13 @@ phoneMenu.addEventListener("click",(e)=>{
         navBar.style.display="none";
     }
 });
+
+// links.forEach((item)=>{
+//     // console.log(item);
+//     let click = document.getElementById(item.getAttribute("data-link"));
+//     console.log(item.getAttribute("data-link"));
+//     console.log(document.getElementById(item.getAttribute("data-link")));
+//     console.log(click);
+//     click.scrollIntoView();
+
+// })
