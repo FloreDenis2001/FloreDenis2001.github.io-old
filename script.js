e325766f282b0dceda199b719f7f69010d527a7a
FloreDenis2001 <floredenis907@yahoo.com>
2023-05-14 01:34:06 +0300

    last_update_add_light_mode

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ let navBar=document.querySelector(".nav-bar");
 let links=document.querySelectorAll(".nav-link");
 let contactPhone=document.querySelectorAll(".contact");
 
+function darkMode(){
+document.documentElement.style.setProperty('--black-to-white','#222831');
+document.documentElement.style.setProperty('--black-to-blue','#222831');
+document.documentElement.style.setProperty('--blue-to-black','#00ADB5');
+document.documentElement.style.setProperty('--blue-to-white','#00ADB5');
+document.documentElement.style.setProperty('--white-to-blue','#E8F6EF');
+document.documentElement.style.setProperty('--white-to-black','#E8F6EF');
+document.documentElement.style.setProperty('--black-to-grey','#222831');
+document.documentElement.style.setProperty('--blue-to-grey','#00ADB5');
+document.documentElement.style.setProperty('--white-to-grey','#ffffff');
+}
+
+function lightMode(){
+  document.documentElement.style.setProperty('--black-to-white','#E8F6EF');
+  document.documentElement.style.setProperty('--black-to-blue','#00ADB5');
+  document.documentElement.style.setProperty('--blue-to-black','#222831');
+  document.documentElement.style.setProperty('--blue-to-white','#E8F6EF');
+  document.documentElement.style.setProperty('--white-to-blue','#00ADB5');
+  document.documentElement.style.setProperty('--white-to-black','#222831');
+  document.documentElement.style.setProperty('--black-to-grey','#F0EEED');
+  document.documentElement.style.setProperty('--blue-to-grey','#F0EEED');
+  document.documentElement.style.setProperty('--white-to-grey','#F0EEED');
+
+}
+
+
+
 contactPhone.forEach((contactPhone)=>{
     
     contactPhone.addEventListener("click",(e)=>{
@@ -75,4 +102,23 @@ function sendEmail() {
     }
   });
 }
+
+
+const colorIcon=document.querySelector('.change-light');
+
+
+let isColorBlack=false;
+
+colorIcon.addEventListener('click',()=>{
+  if(isColorBlack){
+     lightMode();
+    isColorBlack=false;
+  }else {
+     darkMode();
+    isColorBlack=true;
+  }
+})
+
+
+darkMode();
 
